7071deb197d5cbb5f962d387b97e9952a1c668a6
S10027198 <alex.k.parsons74@gmail.com>
2023-09-29 10:32:00 -0500

    added other screens and moving between screens

--- main.py
+++ main.py
@@ -1,16 +1,42 @@
 import kivy
 from kivy.app import App
+from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.uix.boxlayout import BoxLayout
 
-class LoginPageApp(App):
-    pass
+Builder.load_file("QuizPage.kv")
 
-class LoginManager(ScreenManager):
+class QuizPageApp(App):
+    def build(self):
+        return QuizManager()
+
+class QuizManager(ScreenManager):
     pass
 
 class Question1Screen(Screen,BoxLayout):
-    pass
+    def answer_question(self,bool):
+        if bool:
+            self.manager.current = "correct"
+        else:
+            self.manager.current = "wrong"
+    def move_screen(self):
+        self.manager.current = "question2"
+
+class Question2Screen(Screen,BoxLayout):
+    def answer_question(self,bool):
+        if bool:
+            self.manager.current = "correct"
+        else:
+            self.manager.current = "wrong"
 
 
-LoginPageApp().run()
+class WrongScreen(Screen,BoxLayout):
+    def move_screen(self):
+        self.manager.current = "question2"
+
+class CorrectScreen(Screen,BoxLayout):
+    def move_screen(self):
+        self.manager.current = "question2"
+
+
+QuizPageApp().run()
